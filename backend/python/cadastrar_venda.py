d9cc60a49c986f1a1ca4aa7b8c3846acb810e95c
pedro-lucassm <pedromagalhaesofc2004@gmail.com>
2026-09-16 22:26:55 -0300

    gitignore

---
+++ backend/python/cadastrar_venda.py
@@ -0,0 +1,7 @@
+from vendas import criar_venda,excluir_venda
+
+# Parametros: cliente, moto_modelo, moto_placa, servico_produto, valor
+
+criar_venda('Junior', 'Fazer 250', 'TY456', 'Revisão Geral', 490.00)
+
+#excluir_venda()
